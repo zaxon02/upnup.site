2fd588ce4f109875ec5cda9d7c71f2b9c918af5f
zaxon02 <zaxon02@gmail.com>
2023-06-20 16:11:19 +0300

    Fix bg

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -6,14 +6,12 @@
 
 @section('content')
     <!-- Page Header-->
-    <header class="masthead" style="background-image: url('https://cdn.shazoo.ru/c576x256/516934_wKz2tdsiaB_6.jpg')">
+    <header class="masthead" style="background-image: url('/assets/img/payment-bg.jpg')">
         <div class="container position-relative px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-md-10 col-lg-8 col-xl-7">
                     <div class="page-heading">
                         <h1>Оплата</h1>
-                        <span class="subheading">Если каждый месяц откладывать понемногу, то уже через год вы будете
-                            удивлены, как мало у вас набралось.</span>
                     </div>
                 </div>
             </div>
@@ -24,8 +22,6 @@
         <div class="container px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-md-10 col-lg-8 col-xl-7">
-                    <p>Деньги не сделают вас счастливее. У меня сейчас 50 миллионов, и я так же счастлив, как и тогда,
-                        когда у меня было 48 миллионов. © Арнольд Шварценеггер</p>
                     <div class="my-5">
                         <form action="https://demo.paykeeper.ru/create/" method="post">
                             @csrf
