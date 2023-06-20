a0c7b26cad1b54e20a60145a63154eaaf6e10be3
zaxon02 <zaxon02@gmail.com>
2023-06-20 16:11:14 +0300

    Change sentence to word in category factory

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -18,7 +18,7 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->sentence(),
+            'name' => $this->faker->word(),
             'image' => $this->faker->image(),
         ];
     }
